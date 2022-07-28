89bb19e08b50777ec65762c9a86093a18bbd7f8c
unknown <xogk1128@naver.com>
2022-07-28 23:06:49 +0900

    nav바 만들기

---
+++ 기초공부/index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".menu_bar");
+const menu = document.querySelector(".menu");
+const icon = document.querySelector(".icon");
+
+btn.addEventListener('click',()=>{
+    menu.classList.toggle('active');
+    icon.classList.toggle('active');
+});
