22d55f56b3f87a7823e899cce306488eec1eb5cd
unknown <xogk1128@naver.com>
2022-09-06 01:16:35 +0900

    querydata의 id 유무에 따른 페이지 표현

--- N_JS복습/main.js
+++ N_JS복습/main.js
@@ -6,10 +6,11 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url,true).query;
     var pathname = url.parse(_url,true).pathname;
-    var title = queryData.id;
     
     if(pathname == '/'){
-        fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+        if(queryData.id === undefined){
+            var title = 'Welcome';
+            var description = 'Hello NodeJS...';
             var template = `
                 <!doctype html>
                 <html>
@@ -31,7 +32,33 @@ var app = http.createServer(function(request,response){
             `
             response.writeHead(200);
             response.end(template);
-        });
+        } else {
+            fs.readFile(`data/${queryData.id}`,'utf-8',function(err, description){
+                var title = queryData.id;
+                var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="?id=HTML">HTML</a></li>
+                        <li><a href="?id=CSS">CSS</a></li>
+                        <li><a href="?id=Javascript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    ${description}
+                    </body>
+                    </html>
+                `
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+        
     } else {
         response.writeHead(404);
         response.end('Not Found');
