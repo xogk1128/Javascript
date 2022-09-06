b8fc29edf3885e71fba8537b18fbb4b0ecff90f3
unknown <xogk1128@naver.com>
2022-09-06 17:07:14 +0900

    함수를 이용한 중복 제거

--- N_JS복습/main.js
+++ N_JS복습/main.js
@@ -2,6 +2,35 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+function templateHTML(title, list, body){
+    return `
+        <!doctype html>
+            <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    ${list}
+                </ol>
+                ${body}
+            </body>
+        </html>
+    `
+}
+
+function templateList(filelist){
+    var i=0;
+    var list ='';
+    while(filelist.length > i){
+        list += `<li><a href="?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        i++;
+    }
+    return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url,true).query;
@@ -13,29 +42,9 @@ var app = http.createServer(function(request,response){
             fs.readdir('./data','utf-8',function(err, filelist){
                 var title = 'Welcome';
                 var description = 'Hello NodeJS...';
-                var i=0;
-                var list ='';
-                while(filelist.length > i){
-                    list += `<li><a href="?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                    i++;
-                }
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    ${list}
-                </ol>
-                <h2>${title}</h2>
-                ${description}
-                </body>
-                </html>
-                `
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+
                 response.writeHead(200);
                 response.end(template);
             });
@@ -43,30 +52,9 @@ var app = http.createServer(function(request,response){
             fs.readdir(`./data`,'utf-8',function(err2, filelist){
                 fs.readFile(`data/${queryData.id}`,'utf-8',function(err, description){
                     var title = queryData.id;
-                
-                    var i=0;
-                    var list ='';
-                    while(filelist.length > i){
-                        list += `<li><a href="?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                        i++;
-                    }
-                    var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        ${list}
-                    </ol>
-                    <h2>${title}</h2>
-                    ${description}
-                    </body>
-                    </html>
-                    `
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+
                     response.writeHead(200);
                     response.end(template);
                 });
