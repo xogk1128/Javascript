f35801bf41ad7883e420cd4a5fb15aa358ae8cd8
Taeha Kim <xogk1128@naver.com>
2023-01-08 16:14:06 +0900

    DOM study

---
+++ Udemy/Dom.js
@@ -0,0 +1,12 @@
+let getId = document.getElementById('');
+document.getElementsByTagName('');
+document.getElementsByClassName('');
+document.querySelector('');
+document.querySelectorAll('');
+
+getId.innerHTML = '';
+getId.innerText = '';
+getId.textContent = '';
+
+getId.setAttribute('class', 'new');
+getId.setAttribute('type');
