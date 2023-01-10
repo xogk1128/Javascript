913ebbdd536399d06590689e46ffb90d9ad6beb8
Taeha Kim <xogk1128@naver.com>
2023-01-10 16:02:42 +0900

    Code Refactoring

--- Udemy/ScoreAdmin_Project/app.js
+++ Udemy/ScoreAdmin_Project/app.js
@@ -1,43 +1,44 @@
-let i = 0;
-let j = 0;
 let winningScore = 3;
 let gameOver = false;
+
+const p1 = {
+    score : 0,
+    button : document.getElementById('first'),
+    display : document.getElementById('player1')
+}
+
+const p2 = {
+    score : 0,
+    button : document.getElementById('second'),
+    display : document.getElementById('player2')
+}
+
+function updateScore(player, opponent){
+    if(!gameOver){
+        player.score += 1;
+        if(player.score==winningScore){
+            gameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+    
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+    }
+    player.display.textContent = player.score;
+}
 
 let select = document.getElementById('winScore');
 select.addEventListener('change', function(e){
     winningScore = parseInt(this.value);
     reset();
-})
-
-
-const p1Score = document.getElementById('player1');
-const p2Score = document.getElementById('player2');
-
-const first = document.getElementById('first');
-first.addEventListener('click', function(e){
-    if(!gameOver){
-        p1Score.innerText = ++i;
-        if(i==winningScore){
-            gameOver = true;
-            p1Score.classList.add('has-text-success');
-            p2Score.classList.add('has-text-danger');
-    
-            first.disabled = true;
-            second.disabled = true;
-        }
-    }
 });
-const second = document.getElementById('second');
-second.addEventListener('click', function(e){
-    p2Score.innerText = ++j;
-    if(j==winningScore){
-        gameOver = true;
-        p1Score.classList.add('has-text-danger');
-        p2Score.classList.add('has-text-success');
-        
-        first.disabled = true;
-        second.disabled = true;
-    }
+
+p1.button.addEventListener('click', function(e){
+    updateScore(p1, p2);
+});
+p2.button.addEventListener('click', function(e){
+    updateScore(p2, p1);
 });
 
 const resetBtn = document.getElementById('reset');
@@ -45,12 +46,11 @@ resetBtn.addEventListener('click', reset);
 
 function reset(){
     gameOver = false;
-    p1Score.innerText = i = 0;
-    p2Score.innerText = j = 0;
 
-    p1Score.classList.remove('has-text-success', 'has-text-danger');
-    p2Score.classList.remove('has-text-success', 'has-text-danger');
-    
-    first.disabled = false;
-    second.disabled = false;
+    for(let p of [p1,p2]){
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
 }
