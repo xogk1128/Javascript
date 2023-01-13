f63849c8b9d3bbe4076eab06f3aec22cf6887c98
Taeha Kim <xogk1128@naver.com>
2023-01-13 14:37:13 +0900

    make Promise

--- Udemy/CallBack/prac.js
+++ Udemy/CallBack/prac.js
@@ -3,14 +3,32 @@ const fakeRequest = (url) => {
         const rand = Math.random();
         setTimeout(()=>{
             if(rand < 0.5)
-                resolve();
-            else
-                reject();
+                resolve('Your Fake Data Here');
+            reject('Request Error!');
         }, 1000);
     })
 };
 
-fakeRequest('/dogs/1')
-    .then(()=>{
-        console.log('DONE WITH REQUEST!');
-    })
+// fakeRequest('/dogs/1')
+//     .then((data)=>{
+//         console.log('DONE WITH REQUEST!');
+//         console.log("Data is :", data);
+//     })
+//     .catch((err) => {
+//         console.log("Oh no!", err);
+//     })
+    
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay);
+    });
+}
+
+delayedColorChange('red',1000)
+    .then(()=> delayedColorChange('green',1000))
+    .then(()=> delayedColorChange('blue',1000))
+    .then(()=> delayedColorChange('violet',1000))
