a71775cee3d2e5c9d5781ebcfcce28f8a29d2cb2
Taeha Kim <xogk1128@naver.com>
2023-01-14 19:43:44 +0900

    Search TV drama or show

---
+++ Udemy/TV_Project/app.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = { params : { q: searchTerm}};
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config)
+    makeUmages(res.data);
+});
+
+const makeUmages = (shows) => {
+    for(let result of shows){
+        if(result.show.image){
+            const img = document.createElement('img');
+            img.src= result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
