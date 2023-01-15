58504be2edf8797be66eaaf03652628954c65ec6
Taeha Kim <xogk1128@naver.com>
2023-01-15 16:32:33 +0900

    Not Use Keyword 'class'

--- Udemy/TV_Project/app.js
+++ Udemy/TV_Project/app.js
@@ -1,18 +1,28 @@
 const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function(e){
     e.preventDefault();
+
     const searchTerm = form.elements.query.value;
     const config = { params : { q: searchTerm}};
     const res = await axios.get(`http://api.tvmaze.com/search/shows`, config)
-    makeUmages(res.data);
+    makeImages(res.data);
 });
 
-const makeUmages = (shows) => {
+const list = document.querySelector('#list');
+const makeImages = (shows) => {
+    if(list.childElementCount){
+        const children = list.children;
+        for(let i=0; i<list.childElementCount; i++)
+            list.removeChild(children[i]);
+    }
     for(let result of shows){
         if(result.show.image){
+            const span = document.createElement('span');
             const img = document.createElement('img');
+            
             img.src= result.show.image.medium;
-            document.body.append(img);
+            span.append(img)
+            list.append(span);
         }
     }
 }
