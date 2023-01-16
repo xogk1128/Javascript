c94b2a852b1751b6867c67bcb61189c329b0fb87
Taeha Kim <xogk1128@naver.com>
2023-01-16 16:53:05 +0900

    Make subreddit Demo

--- Udemy/Express/Templating_Demo/index.js
+++ Udemy/Express/Templating_Demo/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -9,14 +11,27 @@ app.get('/', (req, res)=>{
     res.render('home');
 })
 
+app.get('/cats', (req, res)=>{
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+    ]
+    res.render('cats', { cats });
+})
+
 app.get('/r/:subreddit', (req, res)=>{
     const {subreddit} = req.params;
-    res.render('subreddit', {subreddit});
+    const data = redditData[subreddit];
+    if(data){
+        res.render('subreddit', {...data});
+    } else {
+        res.render('notfound', {subreddit});
+    }
+    
 })
 
 app.get('/rand', (req, res)=>{
     const num = Math.floor(Math.random()*10)+1;
-    res.render('random', {rand : num});
+    res.render('random', {num});
 })
 
 app.listen(3000, ()=>{
