edb3ff3dbe98cab0afc2765197ab6db46bf0a6c1
Taeha Kim <xogk1128@naver.com>
2023-01-17 16:01:23 +0900

    CRUD Read

--- Udemy/Express/REST_DEMO/index.js
+++ Udemy/Express/REST_DEMO/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4 : uuid } = require('uuid');
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
@@ -9,18 +10,22 @@ app.set('view engine', 'ejs');
 
 let comments = [
     {
+        id : uuid(),
         username: 'Todd',
         comment: 'lol that is so funny!'
     },
     {
+        id : uuid(),
         username: 'Skyler',
         comment: 'I like to go birdwatching with my dog'
     },
     {
+        id : uuid(),
         username: 'Sk8erBoi',
         comment: 'Plz delete your account, Todd'
     },
     {
+        id : uuid(),
         username: 'onlysayswoof',
         comment: 'woof woof woof'
     }
@@ -36,8 +41,14 @@ app.get('/comments/new', (req, res)=>{
 
 app.post('/comments', (req, res)=>{
     const { username, comment } = req.body;
-    comments.push({username , comment});
+    comments.push({username , comment, id : uuid()});
     res.redirect('/comments');
+});
+
+app.get('/comments/:id', (req,res)=>{
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', {comment});
 });
 
 app.get('/tacos', (req, res)=>{
