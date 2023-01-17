47ace6b6bf9a2542735bc7b2bc4a13c73c475739
Taeha Kim <xogk1128@naver.com>
2023-01-17 16:41:44 +0900

    CRUD Update

--- Udemy/Express/REST_DEMO/index.js
+++ Udemy/Express/REST_DEMO/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { v4 : uuid } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
+app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -49,6 +51,20 @@ app.get('/comments/:id', (req,res)=>{
     const {id} = req.params;
     const comment = comments.find(c => c.id === id);
     res.render('comments/show', {comment});
+});
+
+app.patch('/comments/:id', (req, res)=>{
+    const {id} = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments');
+})
+
+app.get('/comments/:id/edit', (req,res)=>{
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', {comment});
 });
 
 app.get('/tacos', (req, res)=>{
