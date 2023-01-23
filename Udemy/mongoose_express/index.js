517c3f7679f78b0200bf518b9e8e986ccb99a95b
Taeha Kim <xogk1128@naver.com>
2023-01-23 17:26:40 +0900

    Make Edit Page

--- Udemy/mongoose_express/index.js
+++ Udemy/mongoose_express/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const Product = require('./models/product');
 
@@ -17,7 +18,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 // views 디렉토리 설정
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine','ejs');
+
+// 미들웨어
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 // index 라우트 : 모든 상품 이름 보여주기
 app.get('/products', async (req, res)=>{
@@ -35,11 +39,23 @@ app.post('/products', async (req, res)=>{
     res.redirect(`products/${newProduct._id}`);
 });
 
-// 
+// 개별 요소 보기
 app.get('/products/:id', async (req, res)=>{
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/show', {product});
+});
+
+// 업데이트 라우트
+app.get('/products/:id/edit', async (req, res)=>{
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', {product});
+});
+app.put('/products/:id', async (req, res)=>{
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators : true, new : true});
+    res.redirect(`products/${product._id}`);
 });
 
 app.listen(3000, ()=>{
