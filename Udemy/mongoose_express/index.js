b74b15134b7f607e5e5678f806fabf8b37219a81
Taeha Kim <xogk1128@naver.com>
2023-01-23 17:00:20 +0900

    Make show Route

--- Udemy/mongoose_express/index.js
+++ Udemy/mongoose_express/index.js
@@ -17,10 +17,29 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 // views 디렉토리 설정
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}));
 
+// index 라우트 : 모든 상품 이름 보여주기
 app.get('/products', async (req, res)=>{
     const products = await Product.find({});
     res.render('products/index', {products});
+});
+
+// new 라우트 : 상품 추가하기
+app.get('/products/new', (req, res)=>{
+    res.render('products/new');
+});
+app.post('/products', async (req, res)=>{
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`products/${newProduct._id}`);
+});
+
+// 
+app.get('/products/:id', async (req, res)=>{
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', {product});
 });
 
 app.listen(3000, ()=>{
