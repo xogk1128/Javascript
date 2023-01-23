9023f1ce1cbef6b405d5ce2c3a546a775e67b0de
Taeha Kim <xogk1128@naver.com>
2023-01-23 19:38:14 +0900

    Make Delete Route

--- Udemy/mongoose_express/index.js
+++ Udemy/mongoose_express/index.js
@@ -23,6 +23,9 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
+// 카테고리
+const categories = ['fruit', 'vegetable', 'dairy'];
+
 // index 라우트 : 모든 상품 이름 보여주기
 app.get('/products', async (req, res)=>{
     const products = await Product.find({});
@@ -31,7 +34,7 @@ app.get('/products', async (req, res)=>{
 
 // new 라우트 : 상품 추가하기
 app.get('/products/new', (req, res)=>{
-    res.render('products/new');
+    res.render('products/new', {categories});
 });
 app.post('/products', async (req, res)=>{
     const newProduct = new Product(req.body);
@@ -50,12 +53,18 @@ app.get('/products/:id', async (req, res)=>{
 app.get('/products/:id/edit', async (req, res)=>{
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', {product});
+    res.render('products/edit', {product, categories});
 });
 app.put('/products/:id', async (req, res)=>{
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators : true, new : true});
     res.redirect(`products/${product._id}`);
+});
+
+app.delete('/products/:id', async (req, res)=>{
+    const { id } = req.params;
+    const deleteProduct = await Product.findByIdAndDelete(id);
+    res.redirect(`/products`);
 });
 
 app.listen(3000, ()=>{
