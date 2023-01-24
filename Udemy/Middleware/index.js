6a32f45fa9d3b1dea52faa3e8029f24ed3058c6b
Taeha Kim <xogk1128@naver.com>
2023-01-24 21:57:39 +0900

    Set 404 Error

--- Udemy/Middleware/index.js
+++ Udemy/Middleware/index.js
@@ -13,6 +13,11 @@ app.use((req, res, next)=>{
     next();
 });
 
+app.use('/dogs', (req,res,next)=>{
+    console.log('');
+    next();
+})
+
 // app.use((req, res, next)=>{
 //     console.log('THIS IS MY FIRST MIDDLEWARE!!!');
 //     next();
@@ -31,6 +36,10 @@ app.get('/', (req,res)=>{
 app.get('/dogs', (req,res)=>{
     console.log(`Request Time : ${req.requestTime}`);
     res.send('WOOF WOOF!');
+});
+
+app.use((req,res)=>{
+    res.status(404).res.send('NOT FOUND!');
 });
 
 app.listen(3000, ()=>{
