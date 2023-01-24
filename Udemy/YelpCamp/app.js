fbc8c8995f75a50a08bbe570bb892c999013663d
Taeha Kim <xogk1128@naver.com>
2023-01-24 23:35:29 +0900

    Page Styling

--- Udemy/YelpCamp/app.js
+++ Udemy/YelpCamp/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -17,6 +18,7 @@ db.once("open", ()=>{
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 // view 폴더 템플릿, ejs 사용
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
