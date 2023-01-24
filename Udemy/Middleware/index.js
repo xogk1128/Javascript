4a80988fb2855429b5770cb2f9f7c8d760ff183c
Taeha Kim <xogk1128@naver.com>
2023-01-24 21:02:00 +0900

    Practice Middleware

--- Udemy/Middleware/index.js
+++ Udemy/Middleware/index.js
@@ -2,13 +2,34 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-morgan('tiny');
+app.use(morgan('tiny'));
+
+// 사용자 미들웨어 만들기
+app.use((req, res, next)=>{
+    // GET을 제외한 요청을 보내면 GET으로 바꿈
+    // req.method = 'GET';
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+});
+
+// app.use((req, res, next)=>{
+//     console.log('THIS IS MY FIRST MIDDLEWARE!!!');
+//     next();
+// });
+
+// app.use((req, res, next)=>{
+//     console.log('THIS IS MY SECOND MIDDLEWARE!!!');
+//     next();
+// });
 
 app.get('/', (req,res)=>{
+    console.log(`Request Time : ${req.requestTime}`);
     res.send('Home Page!');
 });
 
 app.get('/dogs', (req,res)=>{
+    console.log(`Request Time : ${req.requestTime}`);
     res.send('WOOF WOOF!');
 });
 
