6d0f0f0847d3bcdcc423c53963d1ace4861d3d15
Taeha Kim <xogk1128@naver.com>
2023-01-25 15:21:52 +0900

    Verify Mongoose Error

--- Udemy/mongoose_express/index.js
+++ Udemy/mongoose_express/index.js
@@ -28,18 +28,14 @@ app.use(methodOverride('_method'));
 const categories = ['fruit', 'vegetable', 'dairy'];
 
 // index 라우트 : 모든 상품 이름 보여주기
-app.get('/products', async (req, res, next)=>{
-    try{
-        const {category} = req.query;
-        if(category){
-            const products = await Product.find({category});
-            res.render('products/index', {products, category});
-        } else {
-            const products = await Product.find({});
-            res.render('products/index', {products, category : 'All'});
-        }
-    } catch(e){
-        next(e);
+app.get('/products', async (req, res)=>{
+    const {category} = req.query;
+    if(category){
+        const products = await Product.find({category});
+        res.render('products/index', {products, category});
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', {products, category : 'All'});
     }
 });
 
@@ -47,60 +43,56 @@ app.get('/products', async (req, res, next)=>{
 app.get('/products/new', (req, res)=>{
     res.render('products/new', {categories});
 });
-app.post('/products', async (req, res, next)=>{
-    try{
-        const newProduct = new Product(req.body);
-        await newProduct.save();
-        res.redirect(`products/${newProduct._id}`);
-    } catch(e){
-        next(e);
-    }
+app.post('/products', async (req, res)=>{
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`products/${newProduct._id}`);
 });
+
+function wrapAsync(fn){
+    return function(req, res, next){
+        fn(req, res, next).catch(e=> next(e));
+    }
+}
 
 // 개별 요소 보기
-app.get('/products/:id', async (req, res, next)=>{
-    try{
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        if(!product){
-            return next(new AppError('Product Not Found!', 403));
-        }
-        res.render('products/show', {product});
-    } catch(e){
-        next(e);
+app.get('/products/:id', wrapAsync(async (req, res, next)=>{
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if(!product){
+        return next(new AppError('Product Not Found!', 403));
     }
-});
+    res.render('products/show', {product});
+}));
 
 // 업데이트 라우트
-app.get('/products/:id/edit', async (req, res, next)=>{
-    try{
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.render('products/edit', {product, categories});
-    } catch(e){
-        next(e);
-    }
+app.get('/products/:id/edit', async (req, res)=>{
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', {product, categories});
 });
-app.put('/products/:id', async (req, res, next)=>{
-    try{
-        const { id } = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body, {runValidators : true, new : true});
-        res.redirect(`products/${product._id}`);
-    } catch(e){
-        next(e);
-    }
-    
+app.put('/products/:id', async (req, res)=>{
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators : true, new : true});
+    res.redirect(`products/${product._id}`);
 });
 
-app.delete('/products/:id', async (req, res, next)=>{
-    try{
-        const { id } = req.params;
-        const deleteProduct = await Product.findByIdAndDelete(id);
-        res.redirect(`/products`);
-    } catch(e){
-        next(e);
-    }
+app.delete('/products/:id', async (req, res)=>{
+    const { id } = req.params;
+    const deleteProduct = await Product.findByIdAndDelete(id);
+    res.redirect(`/products`);
 });
+
+const handleValidationErr = err => {
+    console.dir(err);
+    return new AppError(`Validation Failed... ${err.message}`, 400)
+}
+
+app.use((err, req, res, next)=>{
+    console.log(err.name);
+    if(err.name === 'ValidationError') err = handleValidationErr(err);
+    next(err);
+})
 
 app.use((err, req, res ,next)=>{
     const {status=500, message='Something went wrong'} = err;
