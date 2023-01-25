23438a9bc7fdb0d21fca4e49aefc9da1369f80b1
Taeha Kim <xogk1128@naver.com>
2023-01-25 14:18:22 +0900

    Put try .. catch

--- Udemy/mongoose_express/index.js
+++ Udemy/mongoose_express/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const AppError = require('./AppError');
 
 const Product = require('./models/product');
 
@@ -27,14 +28,18 @@ app.use(methodOverride('_method'));
 const categories = ['fruit', 'vegetable', 'dairy'];
 
 // index 라우트 : 모든 상품 이름 보여주기
-app.get('/products', async (req, res)=>{
-    const {category} = req.query;
-    if(category){
-        const products = await Product.find({category});
-        res.render('products/index', {products, category});
-    } else {
-        const products = await Product.find({});
-        res.render('products/index', {products, category : 'All'});
+app.get('/products', async (req, res, next)=>{
+    try{
+        const {category} = req.query;
+        if(category){
+            const products = await Product.find({category});
+            res.render('products/index', {products, category});
+        } else {
+            const products = await Product.find({});
+            res.render('products/index', {products, category : 'All'});
+        }
+    } catch(e){
+        next(e);
     }
 });
 
@@ -42,38 +47,65 @@ app.get('/products', async (req, res)=>{
 app.get('/products/new', (req, res)=>{
     res.render('products/new', {categories});
 });
-app.post('/products', async (req, res)=>{
-    const newProduct = new Product(req.body);
-    await newProduct.save();
-    res.redirect(`products/${newProduct._id}`);
+app.post('/products', async (req, res, next)=>{
+    try{
+        const newProduct = new Product(req.body);
+        await newProduct.save();
+        res.redirect(`products/${newProduct._id}`);
+    } catch(e){
+        next(e);
+    }
 });
 
 // 개별 요소 보기
-app.get('/products/:id', async (req, res)=>{
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.render('products/show', {product});
+app.get('/products/:id', async (req, res, next)=>{
+    try{
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if(!product){
+            return next(new AppError('Product Not Found!', 403));
+        }
+        res.render('products/show', {product});
+    } catch(e){
+        next(e);
+    }
 });
 
 // 업데이트 라우트
-app.get('/products/:id/edit', async (req, res)=>{
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.render('products/edit', {product, categories});
+app.get('/products/:id/edit', async (req, res, next)=>{
+    try{
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.render('products/edit', {product, categories});
+    } catch(e){
+        next(e);
+    }
 });
-app.put('/products/:id', async (req, res)=>{
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators : true, new : true});
-    res.redirect(`products/${product._id}`);
+app.put('/products/:id', async (req, res, next)=>{
+    try{
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body, {runValidators : true, new : true});
+        res.redirect(`products/${product._id}`);
+    } catch(e){
+        next(e);
+    }
+    
 });
 
-app.delete('/products/:id', async (req, res)=>{
-    const { id } = req.params;
-    const deleteProduct = await Product.findByIdAndDelete(id);
-    res.redirect(`/products`);
+app.delete('/products/:id', async (req, res, next)=>{
+    try{
+        const { id } = req.params;
+        const deleteProduct = await Product.findByIdAndDelete(id);
+        res.redirect(`/products`);
+    } catch(e){
+        next(e);
+    }
 });
 
-
+app.use((err, req, res ,next)=>{
+    const {status=500, message='Something went wrong'} = err;
+    req.status(status).send(message);
+})
 
 app.listen(3000, ()=>{
     console.log('APP IS LISTENING ON PORT 3000!');
