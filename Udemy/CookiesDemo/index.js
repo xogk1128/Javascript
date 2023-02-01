583dc985547b4d9b3dee7901cfed113d5828c288
Taeha Kim <xogk1128@naver.com>
2023-02-01 23:47:08 +0900

    Use cookie-parser middleware

--- Udemy/CookiesDemo/index.js
+++ Udemy/CookiesDemo/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 app.get('/greet', (req,res)=>{
-    res.send("Hey there!");
+    const { name = 'No-name' } = req.cookies;
+    res.send(`Hey there, ${name}`);
 })
 
 app.get('/setName', (req,res)=>{
