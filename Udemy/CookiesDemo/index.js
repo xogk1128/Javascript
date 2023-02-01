a95a4f0baa3dd07ca18fec194a5f654cd7504aa1
Taeha Kim <xogk1128@naver.com>
2023-02-02 00:00:39 +0900

    Sign Cookies

--- Udemy/CookiesDemo/index.js
+++ Udemy/CookiesDemo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(cookieParser('thisismysecret'));
 
 app.get('/greet', (req,res)=>{
     const { name = 'No-name' } = req.cookies;
@@ -13,6 +13,17 @@ app.get('/setName', (req,res)=>{
     res.cookie('name', 'stevie chicks');
     res.send('OK SENT YOU A COOKIE!!');
 })
+
+app.get('/getsignedcookie', (req,res)=>{
+    res.cookie('fruit', 'grape', {signed : true});
+    res.send('OK SIGNED YOUR FRUIT COOKIE!!');
+})
+
+app.get('/verifyfruit', (req,res)=>{
+    console.log(req.cookies);
+    res.send(req.cookies);
+})
+
 
 app.listen(3000, ()=>{
     console.log('Serving');
