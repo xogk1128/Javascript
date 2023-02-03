fa3e9c270149ce741fc6d6c7ebe58f83395be870
Taeha Kim <xogk1128@naver.com>
2023-02-03 21:41:33 +0900

    About Session

--- Udemy/SessionDemo/index.js
+++ Udemy/SessionDemo/index.js
@@ -2,10 +2,27 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 
-app.use(session({secret:'thisisnotatgoodsecret'}))
+const sessionOptions = { secret:'thisisnotatgoodsecret', resave : false, saveUninitialized : false };
+app.use(session(sessionOptions));
 
 app.get('/viewcount', (req, res)=>{
-    res.send(`YOU HAVE VIEWD THIS PAGE X TIMES`);
+    if(req.session.count){
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`YOU HAVE VIEWD THIS PAGE ${req.session.count} TIMES`);
+});
+
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous'} = req.query;
+    req.session.username = username;
+    res.redirect('/greet');
+});
+
+app.get('/greet', (req, res)=>{
+    const { username } = req.session;
+    res.send(`Hey there, ${username}`);
 })
 
 app.listen(3000, ()=>{
