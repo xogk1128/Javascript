d3ed2aea706b72bff31f8f956d1acdfaacf21bbc
Taeha Kim <xogk1128@naver.com>
2023-02-04 16:49:37 +0900

    Set Sessions

--- Udemy/YelpCamp/app.js
+++ Udemy/YelpCamp/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
+const flash = require('connect-flash');
 //const { campgroundSchema, reviewSchema} = require('./schemas')
 //const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./Utils/ExpressError');
@@ -36,6 +37,24 @@ app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const sessionConfig = {
+    secret : 'thisshouldbeabettersecret!',
+    resave : false,
+    saveUninitialized : true,
+    cookie : {
+        httpOnly : true,
+        expires : Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge : 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next)=>{
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 // 라우트 설정
 app.use('/campgrounds', campgrounds);
