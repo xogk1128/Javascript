4f06d411a20ef641bfb3c3007d5f859507a5e534
Taeha Kim <xogk1128@naver.com>
2023-02-05 16:53:43 +0900

    Make form

--- Udemy/AuthDemo/index.js
+++ Udemy/AuthDemo/index.js
@@ -3,6 +3,15 @@ const app = express();
 
 const User = require('./models/user');
 
+// ejs 사용
+app.set('view engine', 'ejs');
+// views 폴더 사용
+app.set('views', 'views');
+
+app.get('/register', (req, res)=>{
+    res.render('register');
+})
+
 app.get('/secret', (req, res)=>{
     res.send('THIS IS SECRET!');
 })
