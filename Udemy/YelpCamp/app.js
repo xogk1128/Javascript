c90fc589ae49ab945fa4ff83154a9b92ceabf51c
Taeha Kim <xogk1128@naver.com>
2023-02-07 21:13:56 +0900

    User Passport

--- Udemy/YelpCamp/app.js
+++ Udemy/YelpCamp/app.js
@@ -10,6 +10,7 @@ const ExpressError = require('./Utils/ExpressError');
 const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 
 const campgrounds = require('./routes/campgrounds');
@@ -49,13 +50,27 @@ const sessionConfig = {
         maxAge : 1000 * 60 * 60 * 24 * 7
     }
 }
+
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next)=>{
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+app.get('/fakeUser', async (req, res)=>{
+    const user = new User({ email : 'coltttt@gmail.com', username : 'colt' });
+    const newUser = await User.register(user, 'chicken');
+    res.send(newUser);
 })
 
 // 라우트 설정
