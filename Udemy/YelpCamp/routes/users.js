b1c3d61ee527f6142528ccde04d6d89bea650da9
Taeha Kim <xogk1128@naver.com>
2023-02-11 16:27:40 +0900

    Use Controller for Refactoring

--- Udemy/YelpCamp/routes/users.js
+++ Udemy/YelpCamp/routes/users.js
@@ -2,47 +2,18 @@ const express = require('express');
 const router = express.Router({ mergeParams : true});
 const passport = require('passport');
 
-const User = require('../models/user');
+const users = require('../controllers/users');
 
 const catchAsync = require('../Utils/catchAsync');
 
-router.get('/register', (req, res)=>{
-    res.render('users/register');
-});
+router.get('/register', users.renderRegister);
 
-router.post('/register', catchAsync(async (req, res, next)=>{
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({email, username});
-        const registeredUSer = await User.register(user, password);
-        req.login(registeredUSer, err => {
-            if(err) return next(err);
-            req.flash('success','Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
-        })
-        
-    } catch(e){
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
-}));
+router.post('/register', catchAsync(users.register));
 
-router.get('/login', (req, res)=>{
-    res.render('users/login');
-});
+router.get('/login', users.renderLogin);
 
-router.post('/login', passport.authenticate('local', { failureFlash : true, failureRedirect : '/login'}), (req, res)=>{
-    req.flash('success', 'Welcome Back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    res.redirect(redirectUrl);
-});
+router.post('/login', passport.authenticate('local', { failureFlash : true, failureRedirect : '/login'}), users.login );
 
-router.get('/logout', (req, res)=>{
-    req.logout((err) => {
-        if (err) next(err);
-        req.flash("success", "Goodbye 😊");
-        res.redirect("/campgrounds");
-    });
-});
+router.get('/logout', users.logout);
 
 module.exports = router;
