237e2b0a4fc5d2e1577c4a0551014c72617ea957
Taeha Kim <xogk1128@naver.com>
2023-02-18 00:48:05 +0900

    complete YelpCamp

--- Udemy/YelpCamp/app.js
+++ Udemy/YelpCamp/app.js
@@ -12,7 +12,6 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-// const helmet = require('helmet');
 
 const mongoSanitize = require('express-mongo-sanitize');
 
